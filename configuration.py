cd4311f01678fb7db33baa52fcfaa9add350cdc5
fuzzz93 <romi@quassu.ru>
2023-09-11 22:49:44 +0500

    Добавление кода и запросов к БД

---
+++ configuration.py
@@ -0,0 +1,4 @@
+# Роман Мишанов, 8-я когорта — Диплом QA+
+URL_SERVICE = "https://44c2b840-f54f-47d7-bf50-fa3fdced0a1a.serverhub.praktikum-services.ru"
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER_INFO = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,14 @@
+# Роман Мишанов, 8-я когорта — Диплом QA+
+order_body = {
+    "firstName": "Роман",
+    "lastName": "Мишанов",
+    "address": "Москва, Лазоревый проезд, 8",
+    "metroStation": 4,
+    "phone": "+79154095959",
+    "rentTime": 5,
+    "deliveryDate": "2023-07-26",
+    "comment": "Оставьте возле двери",
+    "color": [
+        "BLACK"
+    ]
+}
